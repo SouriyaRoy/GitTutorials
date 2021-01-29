c7fe86f2be34c2ad7898fb1643e01556e6c9b077
Souriya Roy <45315716+SouriyaRoy@users.noreply.github.com>
2021-01-29 15:42:16 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,18 @@
+#add
+#this is for rebasing example in bug1
+def add(x,y):
+    return x+y
+    
+#subtract
+def subtract(x,y):
+    if x<y:
+        return Error
+    return x-y
+    
+#multiply
+def multiply(x,y):
+    return x*y
+    
+#divide
+def divide(x,y):
+    return x/y
